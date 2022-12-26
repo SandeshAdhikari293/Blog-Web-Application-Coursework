285eeb3fe474ba00d079b49d51d4f7903f130afb
SandeshAdhikar-i <2035469@swansea.ac.uk>
2022-12-26 01:01:47 +0000

    Show comments on users page

--- resources/views/posts/users.blade.php
+++ resources/views/posts/users.blade.php
@@ -6,16 +6,28 @@
     @if (session('message'))
         <p><b>{{session('message')}} </b></p>    
     @endif
-
-        @foreach ($user->posts as $post)
-            <ul> 
-                <div style="width:500px;height:100px;border:1px solid #000;">
-                    <ui> <a href="{{ route('posts.show', ['id' => $post->id]) }}"> {{ $post->title }} </a> </ui>
-                    <br><br><br>
-                    <ui> <a href="{{ route('users.show', ['id' => $post->user->id]) }}"> Posted by {{ $post->user->name }} </a> </ui>
-
-                </div>
-            </ul>
-        @endforeach
+        <p>Your posts:</p>
+        <div>
+            @foreach ($user->posts as $post)
+                <ul> 
+                    <div style="width:500px;height:100px;border:1px solid #000;">
+                        <li> <a href="{{ route('posts.show', ['id' => $post->id]) }}"> {{ $post->title }} </a> </li>
+                    </div>
+                </ul>
+            @endforeach
+        </div>
+        <br> <br>
+        <p>Your Comments:</p>
+        <div>
+            @foreach ($user->comments as $comment)
+                <ul> 
+                    <div style="width:500px;height:100px;border:1px solid #000;">
+                        <li> <a href="{{ route('posts.show', ['id' => $comment->post->id]) }}"> {{ $post->title }} </a> </li>
+                        <br>
+                        <p> {{$comment->text}}</p>
+                    </div>
+                </ul>
+            @endforeach
+        </div>
 
 @endsection
