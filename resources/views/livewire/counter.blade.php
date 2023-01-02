6b79d04c2207eeab25d2f4862186209abbcdb0d1
SandeshAdhikar-i <2035469@swansea.ac.uk>
2023-01-02 17:52:22 +0000

    livewire buttons for pages

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,46 +1,55 @@
 <div>
-    @php
-        $count = 0;
-    @endphp
-    @foreach($post->comments as $comment)
-        @if($count < $page * $comment_per_page && $count > ($page * $comment_per_page) - ($comment_per_page + 1) )
-            <div class="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-4 max-w-md md:max-w-2xl ">
-                <div class="flex items-start px-4 py-6">
-                    <div class="">
-                        <p class="text-lg font-semibold text-gray-900 -mt-1">
-                            {{$comment->text}}
-                        </p>
-                        <div class="mt-4 flex items-center">
-                            <div class="flex mr-2 text-gray-700 text-sm mr-4">
-                                <span> <a href= "{{route('users.show', ['id' => $post->user->id, 'ppage' => 1, 'cpage' => 1])}}">posted by {{$comment->user->name}} </a></span>
+    <div class="block bg-gray shadow-lg">
+
+        <br>
+        <h1 class="flex justify-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">View <span class="text-blue-600 dark:text-blue-500">Comments</span></h1>
+        <br>
+                    
+        @php
+            $count = 0;
+        @endphp
+        @foreach($post->comments as $comment)
+            @if($count < $page * $comment_per_page && $count > ($page * $comment_per_page) - ($comment_per_page + 1) )
+                <div class="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-4 max-w-md md:max-w-2xl ">
+                    <div class="flex items-start px-4 py-6">
+                        <div class="">
+                            <p class="text-lg font-semibold text-gray-900 -mt-1">
+                                {{$comment->text}}
+                            </p>
+                            <div class="mt-4 flex items-center">
+                                <div class="flex mr-2 text-gray-700 text-sm mr-4">
+                                    <span> <a href= "{{route('users.show', ['id' => $post->user->id, 'ppage' => 1, 'cpage' => 1])}}">posted by {{$comment->user->name}} </a></span>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <br>
                 </div>
-                <br>
-            </div>
-        @endif
-        @php
-            $count = $count + 1;
-        @endphp
-    @endforeach
-    @if($page > 1)
-        <button wire:click="prev_page">Prev Page</button>
-    @endif
-    
-    @if($page < count($post->comments) / $comment_per_page)
-        <button wire:click="next_page">Next Page</button>
-    @endif
+            @endif
+            @php
+                $count = $count + 1;
+            @endphp
+        @endforeach
+        
+        @if($page > 1)
 
-    <br>
-    <div class="flex justify-center">
-        <div class ="block w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <label for="comment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment to this post?</label>
-                 <textarea wire:model = "input" type ="text" name="comment" id="comment" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your comment..">{{ old('content') }}</textarea>
-                <br>
-                <div class="flex justify-between">
-                    <button type="submit" wire:click="post" id="btn-submit" name="btn-submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 btn-submit">Post comment</button>
-                </div>
+            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" wire:click="prev_page">Prev Page</button>
+        @endif
+
+        @if($page < count($post->comments) / $comment_per_page)
+            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r" wire:click="next_page">Next Page</button>
+        @endif
+
+        <br>
+        <div class="flex justify-center">
+            <div class ="block w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <label for="comment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment to this post?</label>
+                    <textarea wire:model = "input" type ="text" name="comment" id="comment" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your comment..">{{ old('content') }}</textarea>
+                    <br>
+                    <div class="flex justify-between">
+                        <button type="submit" wire:click="post" id="btn-submit" name="btn-submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 btn-submit">Post comment</button>
+                    </div>
+            </div>
         </div>
     </div>
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,16 +8,12 @@
     </head>
     <body>
         @livewireScripts
-    @php
-        $posts_per_page = 3;
-        $count = 0;
-    @endphp
     <form method="GET" action="{{ route('posts.index', ['page' => 1]) }}">
         @csrf
         <button>Go back</button>
     </form>
 
-    <div class="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-8 max-w-md md:max-w-2xl "><!--horizantil margin is just for display-->
+    <div class="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-8 max-w-md md:max-w-2xl">
                 <div class="flex items-start px-4 py-6">
                         @if($post->image != "")
                             <img width="200" height = "200" src="{{ url('image/'. $post->image) }}" alt="">
@@ -53,42 +49,7 @@
                         </div>
                     </div>
                 </div>
-                </div>
-                <br>
-                <h1 class="flex justify-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">View <span class="text-blue-600 dark:text-blue-500">Comments</span></h1>
-                <br>
-                
-<!-- 
-    <div class="flex justify-center">
-        <div class ="block w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h2 class="flex justify-center text-4xl font-extrabold dark:text-white">{{$post->title}}</h2>
-
-        </div>
-    </div> -->
-    <!-- <div style="clear: left;">
-        <br>
-        <p>Comments:</p>    
-        @foreach ($comments as $comment)
-                <br>
-                <ul> 
-                    <div style="width:500px;height:100px;border:1px solid #000;">
-                        <li> {{ $comment->text }} </li>
-                        <br><br>
-                        <li> Comment by <a href="{{ route('users.show', ['id' => $comment->user_id, 'ppage' => 1, 'cpage' => 1]) }}"> {{ $comment->user->name }} </a> </li>
-                
-
-                        @if(auth()->user()->is_admin || auth()->user()->id == $user->id)
-                            <form method="DELETE" action="{{ route('comments.destroy', ['p_id' => $post->id, 'c_id' => $comment->id]) }}">
-                                @csrf                
-                                <input type="submit" value="Delete comment">
-                            </form>
-                        @endif
-
-                    </div>
-                </ul>
-        @endforeach
-        <br><br><br> -->
-
+            </div>
     </div>
 
     @if(auth()->user()->is_admin || auth()->user()->id == $user->id)
@@ -101,7 +62,6 @@
         <p>not Admin log in</p>
     @endif
 
-    <br><br><br>
     @if ($errors->any())
         <div>
             Errors:
@@ -112,36 +72,6 @@
             </ul>
         </div>
     @endif
-    <!-- <div class="flex justify-center">
-        <div class ="block w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <form method="POST" action="{{ route('comments.store', ['id' => $post->id]) }}">
-                @csrf
-
-            <textarea id="content" name="content" rows="4" cols="50">{{ old('content') }}</textarea> -->
-            <!-- </form>
-            <label for="comment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment to this post?</label>
-                <textarea wire:model = "input" type ="text" name="comment" id="comment" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your comment..">{{ old('content') }}</textarea>
-
-                <br>
-                <p>Content: <input type="text" name="content" value = "{{ old('content') }}"></p>
-
-                <input type="submit" value="Submit">
-                <div class="flex justify-between">
-                    <button class="btn btn-success btn-submit">Submit</button>
-                    <button type="submit" wire:click="post(2)" id="btn-submit" name="btn-submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 btn-submit">Post comment</button>
-                </div>
-            </div>
-</div> -->
-<br><br>
-    <!-- <div>
-        <form method="POST" action="{{ route('comments.store', ['id' => $post->id]) }}">
-            @csrf
-            <p>Comment: <input type="text" name="comment" value = "{{ old('comment') }}"></p>
-            
-            <input type="submit" value="Submit">
-            <a href=" {{ route('posts.index', ['page' => 1]) }}">Cancel</a>
-        </form>
-    </div> -->
     @livewire('counter', ['post' => $post])
     </body>
 @endsection
